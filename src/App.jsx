c753673b09eeafac5e3fb76ca420d3bbbf875a1e
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-23 21:44:44 +0530

    understanding routing + react-router + client-side-routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router"
 import './App.css'
+import { Landing } from "./Components/Landing";
+import DashBoard from "./Components/Dashboard";
+import Header from "./Components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // Syntax to enable routing in the application
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+        <Routes>
+          <Route path="" element={<Landing/>}/>
+          <Route path="dashboard" element={<DashBoard/>} />
+        </Routes>
     </>
+
   )
 }
 
 export default App
+
+
+// "React" allows us to make "single page web-applications" (SPA) -> there is no way to naviagte to other pages when application is building using "React", Using React-router we can build" multi-page web-application"
+
+// "React-router" enables "client-side-routing" -> this refers to handling navigation within the browser using the js ( does not cause full page reload ) no request will be send to the server, only using JS we will update the component
+
+/*
+On URL change / navigation happens , React Router:
+
+1. Intercepts the request.
+2. Matches the URL to a route.
+3. Browser URL changes to new Route
+4. React dynamically shows the <About /> component
+5. No network request for a new HTML page
+
+Dynamically renders the appropriate React component within the root element present inside the index.html and hence does not cause the page to reload
+
+// "React-router" uses Client-side-routing to naviagte between different pages. In Client-side-routing we uses "JS" to navigate to different page + updation of Component inside the index.html file. Every time when the user navigates single index.html file gets updates
+
+*/
+
+
+// When doing Client-side-routing we request the resources only once like index.html + css + js. JS / React-router ( ki js) contains all the logic of doing of routing / navigating to different screen ( updates the browser URL + updates the Component that will render inside the root element) => single html that gets updated everytime
---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router"
+
+export default function Header() {
+    const navigate = useNavigate();
+
+    return (
+        <div style={{
+            height: 50, width: "100vw", position: "absolute", top: 0, left: 0, right: 0, backgroundColor: "whitesmoke", color: "gray", display: "flex", gap: 40, justifyContent: "center", padding: 20
+        }}>
+            <button onClick={function() {
+                navigate("")
+            }}>
+                Landing Page
+            </button>
+
+            <button onClick={() => navigate("dashboard")}>
+                DashBoard
+            </button>
+        </div>
+    )
+}
+
+// we can use -> "window.location.href" to naviagate between different pages but this does not enable Client-side-routing instead it causes a full-reload ( sends the request again to the server that again comes with fresh html + css + js )
+
+// using "useNavigate" Hook to enable client-side-routing + this hook can only be used inside the Component which is wrapped inside the BrowserStack
